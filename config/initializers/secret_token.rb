a2e0a84f32c0487b8025617aa6b83430ab899d75
unknown <sirving@.mobileepiphany.local>
2011-05-02 09:45:31 -0600

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1ad067fee956e358f7b2a9ff83da53b16b320aef95e5e384353b47e49ed30198abe55467e2e6254d22be4a7574f52e101a3a38ccf059dd07f6b619092858f7e4'
